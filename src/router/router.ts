d40ebb26ebdb8ec6bea90d968723fa58be5f616a
lidongqi <lidongqi@huohua.cn>
2020-12-18 19:29:27 +0800

    update

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,5 +1,9 @@
-import Index from "../pages/index";
-const router: Array<any> = [
+import Index from "@pages/index";
+interface iRouter {
+  path: string;
+  component: Function;
+}
+const router: Array<iRouter> = [
   {
     path: "/",
     component: Index,
