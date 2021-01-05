c7f1022476e9547ba7eae354c4d1751658b2dbe2
lidongqi <lidongqi@huohua.cn>
2021-01-05 20:17:23 +0800

    update

--- src/component/utils.ts
+++ src/component/utils.ts
@@ -1,5 +1,40 @@
-const add = (): void => {
-  console.log(111);
-};
+/**
+ * 字符串中某个字符出现的次数
+ * @param str
+ */
+function countWord(str: string) {
+  return str.split("").reduce((res, cur) => {
+    res[cur] ? res[cur]++ : (res[cur] = 1);
+    return res;
+  }, {} as Record<string, any>);
+}
 
-export default add;
+/**
+ *
+ * @param arr reduce 数组去重
+ */
+function repetition(arr: number[]): number[] {
+  return arr.reduce((pre, cur) => {
+    if (!pre.includes(cur)) {
+      return pre.concat(cur);
+    } else {
+      return pre;
+    }
+  }, [] as number[]);
+}
+function repetition1(arr: number[]): number[] {
+  const arrTemp = (new Set(arr) as unknown) as number[];
+  return [...arrTemp];
+}
+
+/**
+ * reduce数组扁平化
+ * @param arr
+ */
+function arrFlat(arr: number[]): number[] {
+  return arr.reduce((pre, cur) => {
+    return pre.concat(cur);
+  }, [] as number[]);
+}
+
+export { countWord, repetition, repetition1, arrFlat };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-
+import { repetition1 } from "@components/utils";
 function index(): any {
+  console.log(repetition1([1, 2, 3333, 4, 4, 45, 66, 7, 2]));
   return <div>123dddddd</div>;
 }
 export default index;
